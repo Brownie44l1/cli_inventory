ae4d6f003785faa6102302c062ae0c8f21cdc228
Brownie44l1 <apataomotayo0@gmail.com>
2025-10-30 03:20:59 +0100

    feat:prototype

--- repo/json_repo.go
+++ repo/json_repo.go
@@ -1,6 +1,12 @@
 package repo
 
-import "github.com/Brownie44l1/cli_inventory/model"
+import (
+	"encoding/json"
+	"errors"
+	"os"
+
+	"github.com/Brownie44l1/cli_inventory/model"
+)
 
 type InventoryRepo interface {
 	SaveItem(item model.Item) error
@@ -8,6 +14,75 @@ type InventoryRepo interface {
 	FindBySKU(sku string) (*model.Item, error)
 }
 
-func () SaveItem(item model.Item) error {
-	
+type FileStore struct {
+	//inventoryRepo InventoryRepo
+	filepath      string
+}
+
+func NewFileStore(filepath string) *FileStore {
+	return &FileStore{
+		//inventoryRepo: inventoryRepo,
+		filepath:      filepath,
+	}
+}
+
+func (fs *FileStore) GetAllItems() ([]model.Item, error) {
+	var items []model.Item
+
+	data, err := os.ReadFile(fs.filepath)
+	if os.IsNotExist(err) {
+		return []model.Item{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) > 0 {
+		if err = json.Unmarshal(data, &items); err != nil {
+			return nil, err
+		}
+	}
+	return items, nil
+}
+
+func (fs *FileStore) SaveItem(item model.Item) error {
+	items, err := fs.GetAllItems()
+	if err != nil {
+		return err
+	}
+
+	maxID := 0
+	for _, it := range items {
+		if it.ID > maxID {
+			maxID = it.ID
+		}
+	}
+
+	item.ID = maxID + 1
+	items = append(items, item)
+
+	data, err := json.MarshalIndent(items, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fs.filepath, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (fs *FileStore) FindBySKU(sku string) (*model.Item, error) {
+	items, err := fs.GetAllItems()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range items {
+		if items[i].Sku == sku {
+			return &items[i], nil
+		}
+	}
+	return nil, errors.New("SKU not found")
 }
--- service/inventory.go
+++ service/inventory.go
@@ -1,8 +1,41 @@
 package service
 
-import "github.com/Brownie44l1/cli_inventory/model"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Brownie44l1/cli_inventory/model"
+	"github.com/Brownie44l1/cli_inventory/repo"
+)
 
 type Inventory interface {
 	AddItem(item model.Item) error
-	ListItems()([]model.Item, error)
+	ListItems() ([]model.Item, error)
+}
+
+type InventoryService struct {
+	repo repo.InventoryRepo
+}
+
+func NewInventoryService(r repo.InventoryRepo) *InventoryService {
+	return &InventoryService{repo: r}
+}
+
+func (s *InventoryService) AddItem(item model.Item) error {
+	if item.Sku == "" || item.Name == "" {
+		return errors.New("missing required field")
+	}
+	
+	if item.Quantity < 0 {
+		return errors.New("invalid quantity")
+	}
+	
+	found, err := s.repo.FindBySKU(item.Sku)
+    if err == nil && found != nil {
+        return fmt.Errorf("an item with SKU %s already exists", item.Sku)
+    }
+    if err != nil && err.Error() != "SKU not found" {
+        return err
+    }
+	return s.repo.SaveItem(item)
 }
